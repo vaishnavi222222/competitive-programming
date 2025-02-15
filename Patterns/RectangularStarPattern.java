9bce6a0ee069267d193ca7db8d6bc6798c1c0299
vaishnavi222222 <151459102+vaishnavi222222@users.noreply.github.com>
2025-02-15 11:43:43 +0530

    Update RectangularStarPattern.java

--- Patterns/RectangularStarPattern.java
+++ Patterns/RectangularStarPattern.java
@@ -1,1 +1,20 @@
+//  basic pattern 
+import java.io.*;
+import java.util.*;
 
+public class RectangularStarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter N value:");
+        int n = sc.nextInt();
+        printpattern(n);
+    }
+    public static void printpattern(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
