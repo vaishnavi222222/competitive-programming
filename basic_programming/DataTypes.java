23a1514951deb93c20aecb7c0e67a9b1f6793060
vaishnavi222222 <151459102+vaishnavi222222@users.noreply.github.com>
2025-02-21 11:46:36 +0530

    Delete basic_programming directory

--- basic_programming/DataTypes.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    static int dataTypeSize(String str) {
-        if(str.equals("Character"))return 1;
-        else if(str.equals("Integer") || str.equals("Float")) return 4;
-        else  return 8;
-    }
-}
--- basic_programming/Pass_by_reference.java
+++
@@ -1,8 +0,0 @@
-class Pass_by_reference {
-    static int[] passedBy(int a, int b) {
-        a = a+1;
-        b = b+2;
-        int ar[] = {a,b};
-        return ar;
-    }
-}
